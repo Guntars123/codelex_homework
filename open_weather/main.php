0028654e6ee287c655acc1c8f75bf063fbfef007
Guntars <gvaivods@gmail.com>
2023-04-04 15:30:46 +0300

    finished

--- open_weather/main.php
+++ open_weather/main.php
@@ -1,1 +1,26 @@
 <?php
+
+echo "Sveicināti mūsu laikapstākļu aplikācijā!" . PHP_EOL;
+
+$apiKey = "71a8de1518d0f32f4ce1f6c52be94a15";
+$cityName = readline("Ievadiet pilsētu: ");
+$response = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q={$cityName}&lang=la&appid={$apiKey}&units=metric");
+
+if ($response === false) {
+    echo "---------------------------------------------------------------------" . PHP_EOL;
+    echo "Something went wrong!" . PHP_EOL;
+    echo "---------------------------------------------------------------------" . PHP_EOL;
+    exit;
+}
+
+$cityData = json_decode($response);
+
+echo "---------------------------------------------------------------------" . PHP_EOL;
+echo "Laikapstākļi pilsētā $cityName:" . PHP_EOL;
+print "* Gaisa temperatūra: $cityData->main->temp} C, pēc sajūtām {$cityData->main->feels_like} C" . PHP_EOL;
+print "* Ārā {$cityData->weather[0]->description} " . PHP_EOL;
+print "* Gaisa spiediens: {$cityData->main->pressure} h/pa" . PHP_EOL;
+print "* Gaisa mitrums: {$cityData->main->humidity} %" . PHP_EOL;
+print "* Redzamība: {$cityData->visibility} m" . PHP_EOL;
+print "* Vēja ātrums: {$cityData->wind->speed} m/s, brāzmās līdz: {$cityData->wind->gust} m/s" . PHP_EOL;
+
